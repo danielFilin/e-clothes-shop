7a0569df80921620ed49c8af9fee530ddec70264
Daniel <filind85@gmail.com>
2021-06-17 16:11:13 +0300

    added checkout functionality

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SIgnInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Checkout from './pages/checkout/checkout.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selector';
@@ -47,8 +48,9 @@ class App extends React.Component {
        <div className="App">
          <Header/>
         <Switch>
-          <Route exact path="/" component={HomePage}/>
-          <Route path="/shop" component={ShopPage }/>
+          <Route exact path="/" component={ HomePage }/>
+          <Route path="/shop" component={ ShopPage }/>
+          <Route exact path="/checkout" component={ Checkout }/>
           <Route exact path='/signin' render={() => this.props.currentUser ? (
           <Redirect to='/' /> ) : ( <SIgnInAndSignUpPage />
           )
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -16,4 +16,10 @@ export const selectCartItemsCount = createSelector(
   [selectCartItems],
   cartItems => 
     cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+);
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems => 
+  cartItems.reduce((accumulatedPrice, cartItem) => accumulatedPrice + cartItem.quantity * cartItem.price, 0)
 )
